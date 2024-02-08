5b07db5d589dee5d2d85d7ec7a14a8a67934c232
Murillo Santana <mvbentosantana@gmail.com>
2024-02-08 18:00:41 +0100

    little fix

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -8,35 +8,35 @@
 #include "Canvas/inc/Objects/Object.hpp"
 
 //data
-// static float t = 0;
+static float t = 0;
 static canvas::Scene* scene;
-// static unsigned index_light = 0;
+static unsigned index_light = 0;
 
 //callbacks
-// static void callback_idle(void)
-// {
-// 	//data
-// 	const unsigned fr = 60;
-// 	//objects
-// 	for(canvas::objects::Object* object : scene->objects())
-// 	{
-// 		object->rotate({0, float(M_PI) / fr, 0}, false);
-// 	}
-// 	scene->update(true);
-// 	//light
-// 	if(index_light == 1)
-// 	{
-// 		t += 2 * float(M_PI) / fr;
-// 		scene->light().position({0, 0, sinf(t) + 1});
-// 		scene->light().update_shaders();
-// 	}
-// 	if(index_light == 2)
-// 	{
-// 		const canvas::vec3 xl = scene->light().position();
-// 		scene->light().position(canvas::vec3(0, float(M_PI) / fr, 0).quaternion().rotate(xl));
-// 		scene->light().update_shaders();
-// 	}
-// }
+static void callback_idle(void)
+{
+	//data
+	const unsigned fr = 60;
+	//objects
+	for(canvas::objects::Object* object : scene->objects())
+	{
+		object->rotate({0, float(M_PI) / fr, 0}, false);
+	}
+	scene->update(true);
+	//light
+	if(index_light == 1)
+	{
+		t += 2 * float(M_PI) / fr;
+		scene->light().position({0, 0, sinf(t) + 1});
+		scene->light().update_shaders();
+	}
+	if(index_light == 2)
+	{
+		const canvas::vec3 xl = scene->light().position();
+		scene->light().position(canvas::vec3(0, float(M_PI) / fr, 0).quaternion().rotate(xl));
+		scene->light().update_shaders();
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -44,11 +44,10 @@ int main(int argc, char** argv)
 	canvas::Glut app(argc, argv, "shd/");
 	//scene
 	scene = app.scene();
-	examples::scenes::beam_1(scene);
-	// examples::objects::text(scene);
+	examples::objects::text(scene);
 	scene->update(true);
 	//callbacks
-	// app.callback_idle(callback_idle);
+	app.callback_idle(callback_idle);
 	//start
 	app.start();
 	//return
