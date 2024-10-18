df50268805c125bfa59decd7e191023d5444e04d
Murillo Vinicius Bento Santana <mvbentosantana@gmail.com>
2024-10-18 17:03:58 +0200

    getting there

--- Test/inc/scenes/Beam.hpp
+++ Test/inc/scenes/Beam.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 //canvas
+#include "Canvas/inc/Math/vec2.hpp"
 #include "Canvas/inc/Objects/Geometry.hpp"
 
 class Beam : public canvas::objects::Geometry
@@ -12,8 +13,25 @@ public:
 	//destructor
 	~Beam(void);
 
+	//data
+	float offset(float);
+	float offset(void) const;
+
+	float thickness(float);
+	float thickness(void) const;
+
 protected:
 	//buffers
+	void ibo_fill_data(void);
+	void vbo_fill_data(void);
+	void ibo_stroke_data(void);
+	void vbo_stroke_data(void);
 	void buffers_size(void) override;
 	void buffers_data(void) const override;
+
+	//data
+	float m_offset;
+	float m_thickness;
+	canvas::vec2 m_nodes;
+	const static uint32_t nc = 40;
 };
--- Test/src/scenes/Beam.cpp
+++ Test/src/scenes/Beam.cpp
@@ -2,7 +2,7 @@
 #include "Canvas/test/inc/scenes/Beam.hpp"
 
 //constructor
-Beam::Beam(void)
+Beam::Beam(void) : m_offset(0)
 {
 	return;
 }
@@ -13,10 +13,47 @@ Beam::~Beam(void)
 	return;
 }
 
+//data
+float Beam::offset(void) const
+{
+	return m_offset;
+}
+float Beam::offset(float offset)
+{
+	return m_offset = offset;
+}
+
+float Beam::thickness(void) const
+{
+	return m_thickness;
+}
+float Beam::thickness(float thickness)
+{
+	return m_thickness = thickness;
+}
+
 //buffers
-void Beam::buffers_size(void)
+void Beam::ibo_fill_data(void)
 {
 	
+}
+void Beam::vbo_fill_data(void)
+{
+	
+}
+void Beam::ibo_stroke_data(void)
+{
+	
+}
+void Beam::vbo_stroke_data(void)
+{
+	
+}
+void Beam::buffers_size(void)
+{
+	m_ibo_size[2] = 6 * nc * m_fill;
+	m_ibo_size[1] = 4 * (nc + 1) * m_stroke;
+	m_vbo_size[0] = 2 * (nc + 1) * (m_fill + m_stroke);
 }
 void Beam::buffers_data(void) const
 {
