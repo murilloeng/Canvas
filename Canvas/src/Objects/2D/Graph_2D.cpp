76eb51df0c6e95a1d1a3d20e9765f15102dc5fd7
Murillo Vinicius Bento Santana <mvbentosantana@gmail.com>
2024-12-07 00:53:49 +0100

    added tics labels

--- Canvas/src/Objects/2D/Graph_2D.cpp
+++ Canvas/src/Objects/2D/Graph_2D.cpp
@@ -14,8 +14,8 @@ namespace canvas
 		//constructors
 		Graph_2D::Graph_2D(void) : 
 			m_range{0, 1, 0, 1}, 
-			m_tics_font{0, 0}, m_tics_count{9, 9}, m_tics_format{"+.2f", "+.2f"}, 
-			m_tics_line_size{0.02f, 0.02f}, m_tics_label_size{0.02f, 0.02f}
+			m_tics_font{0, 0}, m_tics_count{9, 9}, m_tics_format{"%.2f", "%.2f"}, 
+			m_tics_line_size{0.02f, 0.02f}, m_tics_label_size{0.04f, 0.04f}
 		{
 			return;
 		}
@@ -133,40 +133,44 @@ namespace canvas
 			//vbo data
 			for(uint32_t i = 0; i < n1; i++)
 			{
+				const float x1 = float(i + 1) / (n1 + 1);
 				vbo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 0].m_color = m_frame_color;
 				vbo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 1].m_color = m_frame_color;
 				vbo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 0].m_color = m_frame_color;
 				vbo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 1].m_color = m_frame_color;
-				vbo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 0].m_position = {float(i + 1) / (n1 + 1), 0, 0};
-				vbo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 0].m_position = {float(i + 1) / (n1 + 1), 1, 0};
-				vbo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 1].m_position = {float(i + 1) / (n1 + 1), s1, 0};
-				vbo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 1].m_position = {float(i + 1) / (n1 + 1), 1 - s1, 0};
+				vbo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 0].m_position = {x1, 0, 0};
+				vbo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 0].m_position = {x1, 1, 0};
+				vbo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 1].m_position = {x1, s1, 0};
+				vbo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 1].m_position = {x1, 1 - s1, 0};
 			}
 			for(uint32_t i = 0; i < n2; i++)
 			{
+				const float x2 = float(i + 1) / (n2 + 1);
 				vbo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 0].m_color = m_frame_color;
 				vbo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 1].m_color = m_frame_color;
 				vbo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 0].m_color = m_frame_color;
 				vbo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 1].m_color = m_frame_color;
-				vbo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 0].m_position = {1, float(i + 1) / (n2 + 1), 0};
-				vbo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 0].m_position = {0, float(i + 1) / (n1 + 1), 0};
-				vbo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 1].m_position = {s2, float(i + 1) / (n1 + 1), 0};
-				vbo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 1].m_position = {1 - s2, float(i + 1) / (n1 + 1), 0};
+				vbo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 0].m_position = {1, x2, 0};
+				vbo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 0].m_position = {0, x2, 0};
+				vbo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 1].m_position = {s2, x2, 0};
+				vbo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 1].m_position = {1 - s2, x2, 0};
 			}
 			//ibo data
 			for(uint32_t i = 0; i < n1; i++)
 			{
-				ibo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 0] = m_vbo_index[0] + 4 + 2 * (0 * n1 + 0 * n2) + 2 * i + 0;
-				ibo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 1] = m_vbo_index[0] + 4 + 2 * (0 * n1 + 0 * n2) + 2 * i + 1;
-				ibo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 0] = m_vbo_index[0] + 4 + 2 * (1 * n1 + 1 * n2) + 2 * i + 0;
-				ibo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 1] = m_vbo_index[0] + 4 + 2 * (1 * n1 + 1 * n2) + 2 * i + 1;
+				const uint32_t a = m_vbo_index[0] + 4 + 2 * i;
+				ibo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 0] = a + 2 * (0 * n1 + 0 * n2) + 0;
+				ibo_ptr[2 * (0 * n1 + 0 * n2) + 2 * i + 1] = a + 2 * (0 * n1 + 0 * n2) + 1;
+				ibo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 0] = a + 2 * (1 * n1 + 1 * n2) + 0;
+				ibo_ptr[2 * (1 * n1 + 1 * n2) + 2 * i + 1] = a + 2 * (1 * n1 + 1 * n2) + 1;
 			}
 			for(uint32_t i = 0; i < n2; i++)
 			{
-				ibo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 0] = m_vbo_index[0] + 4 + 2 * (1 * n1 + 0 * n2) + 2 * i + 0;
-				ibo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 1] = m_vbo_index[0] + 4 + 2 * (1 * n1 + 0 * n2) + 2 * i + 1;
-				ibo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 0] = m_vbo_index[0] + 4 + 2 * (2 * n1 + 1 * n2) + 2 * i + 0;
-				ibo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 1] = m_vbo_index[0] + 4 + 2 * (2 * n1 + 1 * n2) + 2 * i + 1;
+				const uint32_t a = m_vbo_index[0] + 4 + 2 * i;
+				ibo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 0] = a + 2 * (1 * n1 + 0 * n2) + 0;
+				ibo_ptr[2 * (1 * n1 + 0 * n2) + 2 * i + 1] = a + 2 * (1 * n1 + 0 * n2) + 1;
+				ibo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 0] = a + 2 * (2 * n1 + 1 * n2) + 0;
+				ibo_ptr[2 * (2 * n1 + 1 * n2) + 2 * i + 1] = a + 2 * (2 * n1 + 1 * n2) + 1;
 			}
 		}
 		void Graph_2D::buffers_data_frame(void) const
@@ -211,140 +215,117 @@ namespace canvas
 			const Font* font_1 = m_scene->font(m_tics_font[0]);
 			const Font* font_2 = m_scene->font(m_tics_font[1]);
 			//vbo data
-			uint32_t counter = 0;
+			uint32_t vbo_counter = 0;
 			for(uint32_t i = 0; i < n1 + 2; i++)
 			{
-				uint32_t nc = sprintf(label, m_tics_format[0], x11 + i * (x12 - x11) / (n1 + 1));
+				float sw = 0;
+				float w2 = -s1;
+				float w1 = float(i) / (n1 + 1);
+				const float x1 = x11 + i * (x12 - x11) / (n1 + 1);
+				uint32_t nc = sprintf(label, m_tics_format[0], x1);
+				for(uint32_t j = 0; j < nc; j++)
+				{
+					//character
+					font_1->character(label[j]).coordinates(tc);
+					const int32_t w = font_1->character(label[j]).width();
+					const int32_t h = font_1->character(label[j]).height();
+					const int32_t r = font_1->character(label[j]).advance();
+					const int32_t a = font_1->character(label[j]).bearing(0);
+					const int32_t b = font_1->character(label[j]).bearing(1);
+					//position
+					xc[2 * 3 + 0] = xc[2 * 0 + 0] = w1 + s1 / font_1->height() * a;
+					xc[2 * 3 + 1] = xc[2 * 2 + 1] = w2 + s1 / font_1->height() * b;
+					xc[2 * 1 + 0] = xc[2 * 2 + 0] = w1 + s1 / font_1->height() * (a + w);
+					xc[2 * 1 + 1] = xc[2 * 0 + 1] = w2 + s1 / font_1->height() * (b - h);
+					//vertices
+					for(uint32_t k = 0; k < 4; k++)
+					{
+						vbo_ptr[4 * vbo_counter + k].m_color = m_frame_color;
+						vbo_ptr[4 * vbo_counter + k].m_texture_coordinates = tc + 2 * k;
+						vbo_ptr[4 * vbo_counter + k].m_position = {xc[2 * k + 0], xc[2 * k + 1], 0};
+					}
+					vbo_counter++;
+					w1 += s1 / font_1->height() * r;
+					sw += s1 / font_1->height() * r;
+				}
 				for(uint32_t j = 0; j < nc; j++)
 				{
 					for(uint32_t k = 0; k < 4; k++)
 					{
-						vbo_ptr[4 * counter + k].m_position = {0, 0, 0};
+						vbo_ptr[4 * (vbo_counter - nc + j) + k].m_position[0] -= sw / 2;
 					}
-					counter++;
 				}
 			}
 			for(uint32_t i = 0; i < n2 + 2; i++)
 			{
-				uint32_t nc = sprintf(label, m_tics_format[1], x21 + i * (x22 - x21) / (n2 + 1));
+				float w1 = 0;
+				float w2 = float(i) / (n2 + 1) - s2 / 2;
+				const float x2 = x21 + i * (x22 - x21) / (n2 + 1);
+				uint32_t nc = sprintf(label, m_tics_format[1], x2);
+				for(uint32_t j = 0; j < nc; j++)
+				{
+					//character
+					font_2->character(label[j]).coordinates(tc);
+					const int32_t w = font_2->character(label[j]).width();
+					const int32_t h = font_2->character(label[j]).height();
+					const int32_t r = font_2->character(label[j]).advance();
+					const int32_t a = font_2->character(label[j]).bearing(0);
+					const int32_t b = font_2->character(label[j]).bearing(1);
+					//position
+					xc[2 * 3 + 0] = xc[2 * 0 + 0] = w1 + s2 / font_2->height() * a;
+					xc[2 * 3 + 1] = xc[2 * 2 + 1] = w2 + s2 / font_2->height() * b;
+					xc[2 * 1 + 0] = xc[2 * 2 + 0] = w1 + s2 / font_2->height() * (a + w);
+					xc[2 * 1 + 1] = xc[2 * 0 + 1] = w2 + s2 / font_2->height() * (b - h);
+					//vertices
+					for(uint32_t k = 0; k < 4; k++)
+					{
+						vbo_ptr[4 * vbo_counter + k].m_color = m_frame_color;
+						vbo_ptr[4 * vbo_counter + k].m_texture_coordinates = tc + 2 * k;
+						vbo_ptr[4 * vbo_counter + k].m_position = {xc[2 * k + 0], xc[2 * k + 1], 0};
+					}
+					vbo_counter++;
+					w1 += s2 / font_2->height() * r;
+				}
 				for(uint32_t j = 0; j < nc; j++)
 				{
 					for(uint32_t k = 0; k < 4; k++)
 					{
-						vbo_ptr[4 * counter + k].m_position = {0, 0, 0};
+						vbo_ptr[4 * (vbo_counter - nc + j) + k].m_position[0] -= w1;
 					}
-					counter++;
 				}
 			}
-			m_scene->font(0)->character('1').coordinates(tc);
-			vbo_ptr[0].m_position = {0, 0, 0};
-			vbo_ptr[1].m_position = {1, 0, 0};
-			vbo_ptr[2].m_position = {1, 1, 0};
-			vbo_ptr[3].m_position = {0, 1, 0};
-			vbo_ptr[0].m_color = m_frame_color;
-			vbo_ptr[1].m_color = m_frame_color;
-			vbo_ptr[2].m_color = m_frame_color;
-			vbo_ptr[3].m_color = m_frame_color;
-			vbo_ptr[0].m_texture_coordinates = tc + 2 * 0;
-			vbo_ptr[1].m_texture_coordinates = tc + 2 * 1;
-			vbo_ptr[2].m_texture_coordinates = tc + 2 * 2;
-			vbo_ptr[3].m_texture_coordinates = tc + 2 * 3;
-			ibo_ptr[3 * 0 + 0] = m_vbo_index[2] + 0;
-			ibo_ptr[3 * 0 + 1] = m_vbo_index[2] + 1;
-			ibo_ptr[3 * 0 + 2] = m_vbo_index[2] + 2;
-			ibo_ptr[3 * 1 + 0] = m_vbo_index[2] + 0;
-			ibo_ptr[3 * 1 + 1] = m_vbo_index[2] + 2;
-			ibo_ptr[3 * 1 + 2] = m_vbo_index[2] + 3;
-			// for(uint32_t i = 0; i < n1 + 2; i++)
-			// {
-			// 	vec3 position = {float(i) / (n1 + 1), 0, 0};
-			// 	uint32_t nc = sprintf(label, m_tics_format[0], x11 + i * (x12 - x11) / (n1 + 1));
-			// 	for(uint32_t j = 0; j < nc; j++)
-			// 	{
-			// 		//character
-			// 		font_1->character(label[j]).coordinates(tc);
-			// 		const int32_t w = font_1->character(label[j]).width();
-			// 		const int32_t h = font_1->character(label[j]).height();
-			// 		const int32_t r = font_1->character(label[j]).advance();
-			// 		const int32_t a = font_1->character(label[j]).bearing(0);
-			// 		const int32_t b = font_1->character(label[j]).bearing(1);
-			// 		//position
-			// 		xc[2 * 0 + 0] = xc[2 * 3 + 0] = position[0] + s1 / font_1->height() * a;
-			// 		xc[2 * 2 + 1] = xc[2 * 3 + 1] = position[1] + s1 / font_1->height() * b;
-			// 		xc[2 * 1 + 0] = xc[2 * 2 + 0] = position[0] + s1 / font_1->height() * (a + w);
-			// 		xc[2 * 0 + 1] = xc[2 * 1 + 1] = position[1] + s1 / font_1->height() * (b - h);
-			// 		//vertices
-			// 		for(uint32_t k = 0; k < 4; k++)
-			// 		{
-			// 			vbo_ptr[k].m_color = m_frame_color;
-			// 			vbo_ptr[k].m_position = {xc[0], xc[1], 0};
-			// 			vbo_ptr[k].m_texture_coordinates = tc + 2 * k;
-			// 		}
-			// 		vbo_ptr += 4;
-			// 		position[0] += s1 / font_1->height() * r;
-			// 	}
-			// }
-			// for(uint32_t i = 0; i < n2 + 2; i++)
-			// {
-			// 	vec3 position = {0, float(i) / (n2 + 1), 0};
-			// 	uint32_t nc = sprintf(label, m_tics_format[1], x21 + i * (x22 - x21) / (n2 + 1));
-			// 	for(uint32_t j = 0; j < nc; j++)
-			// 	{
-			// 		//character
-			// 		font_2->character(label[j]).coordinates(tc);
-			// 		const int32_t w = font_2->character(label[j]).width();
-			// 		const int32_t h = font_2->character(label[j]).height();
-			// 		const int32_t r = font_2->character(label[j]).advance();
-			// 		const int32_t a = font_2->character(label[j]).bearing(0);
-			// 		const int32_t b = font_2->character(label[j]).bearing(1);
-			// 		//position
-			// 		xc[2 * 0 + 0] = xc[2 * 3 + 0] = position[0] + s2 / font_2->height() * a;
-			// 		xc[2 * 2 + 1] = xc[2 * 3 + 1] = position[1] + s2 / font_2->height() * b;
-			// 		xc[2 * 1 + 0] = xc[2 * 2 + 0] = position[0] + s2 / font_2->height() * (a + w);
-			// 		xc[2 * 0 + 1] = xc[2 * 1 + 1] = position[1] + s2 / font_2->height() * (b - h);
-			// 		//vertices
-			// 		for(uint32_t k = 0; k < 4; k++)
-			// 		{
-			// 			vbo_ptr[k].m_color = m_frame_color;
-			// 			vbo_ptr[k].m_position = {xc[0], xc[1], 0};
-			// 			vbo_ptr[k].m_texture_coordinates = tc + 2 * k;
-			// 		}
-			// 		vbo_ptr += 4;
-			// 		position[0] += s2 / font_1->height() * r;
-			// 	}
-			// }
-			// //ibo data
-			// uint32_t counter = 0;
-			// for(uint32_t i = 0; i < n1 + 2; i++)
-			// {
-			// 	uint32_t nc = sprintf(label, m_tics_format[0], x11 + i * (x12 - x11) / (n1 + 1));
-			// 	for(uint32_t j = 0; j < nc; j++)
-			// 	{
-			// 		ibo_ptr[3 * 0 + 0] = m_vbo_index[2] + 4 * counter + 0;
-			// 		ibo_ptr[3 * 0 + 1] = m_vbo_index[2] + 4 * counter + 1;
-			// 		ibo_ptr[3 * 0 + 2] = m_vbo_index[2] + 4 * counter + 2;
-			// 		ibo_ptr[3 * 1 + 0] = m_vbo_index[2] + 4 * counter + 0;
-			// 		ibo_ptr[3 * 1 + 1] = m_vbo_index[2] + 4 * counter + 2;
-			// 		ibo_ptr[3 * 1 + 2] = m_vbo_index[2] + 4 * counter + 3;
-			// 		counter++;
-			// 		ibo_ptr += 6;
-			// 	}
-			// }
-			// for(uint32_t i = 0; i < n2 + 2; i++)
-			// {
-			// 	uint32_t nc = sprintf(label, m_tics_format[1], x21 + i * (x22 - x21) / (n2 + 1));
-			// 	for(uint32_t j = 0; j < nc; j++)
-			// 	{
-			// 		ibo_ptr[3 * 0 + 0] = m_vbo_index[2] + 4 * counter + 0;
-			// 		ibo_ptr[3 * 0 + 1] = m_vbo_index[2] + 4 * counter + 1;
-			// 		ibo_ptr[3 * 0 + 2] = m_vbo_index[2] + 4 * counter + 2;
-			// 		ibo_ptr[3 * 1 + 0] = m_vbo_index[2] + 4 * counter + 0;
-			// 		ibo_ptr[3 * 1 + 1] = m_vbo_index[2] + 4 * counter + 2;
-			// 		ibo_ptr[3 * 1 + 2] = m_vbo_index[2] + 4 * counter + 3;
-			// 		counter++;
-			// 		ibo_ptr += 6;
-			// 	}
-			// }
+			//ibo data
+			uint32_t ibo_counter = 0;
+			for(uint32_t i = 0; i < n1 + 2; i++)
+			{
+				const float x1 = x11 + i * (x12 - x11) / (n1 + 1);
+				uint32_t nc = sprintf(label, m_tics_format[0], x1);
+				for(uint32_t j = 0; j < nc; j++)
+				{
+					ibo_ptr[6 * ibo_counter + 3 * 0 + 0] = m_vbo_index[2] + 4 * ibo_counter + 0;
+					ibo_ptr[6 * ibo_counter + 3 * 0 + 1] = m_vbo_index[2] + 4 * ibo_counter + 1;
+					ibo_ptr[6 * ibo_counter + 3 * 0 + 2] = m_vbo_index[2] + 4 * ibo_counter + 2;
+					ibo_ptr[6 * ibo_counter + 3 * 1 + 0] = m_vbo_index[2] + 4 * ibo_counter + 0;
+					ibo_ptr[6 * ibo_counter + 3 * 1 + 1] = m_vbo_index[2] + 4 * ibo_counter + 2;
+					ibo_ptr[6 * ibo_counter + 3 * 1 + 2] = m_vbo_index[2] + 4 * ibo_counter + 3;
+					ibo_counter++;
+				}
+			}
+			for(uint32_t i = 0; i < n2 + 2; i++)
+			{
+				const float x2 = x21 + i * (x22 - x21) / (n2 + 1);
+				uint32_t nc = sprintf(label, m_tics_format[1], x2);
+				for(uint32_t j = 0; j < nc; j++)
+				{
+					ibo_ptr[6 * ibo_counter + 3 * 0 + 0] = m_vbo_index[2] + 4 * ibo_counter + 0;
+					ibo_ptr[6 * ibo_counter + 3 * 0 + 1] = m_vbo_index[2] + 4 * ibo_counter + 1;
+					ibo_ptr[6 * ibo_counter + 3 * 0 + 2] = m_vbo_index[2] + 4 * ibo_counter + 2;
+					ibo_ptr[6 * ibo_counter + 3 * 1 + 0] = m_vbo_index[2] + 4 * ibo_counter + 0;
+					ibo_ptr[6 * ibo_counter + 3 * 1 + 1] = m_vbo_index[2] + 4 * ibo_counter + 2;
+					ibo_ptr[6 * ibo_counter + 3 * 1 + 2] = m_vbo_index[2] + 4 * ibo_counter + 3;
+					ibo_counter++;
+				}
+			}
 		}
 	}
 }
