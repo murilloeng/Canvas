306598faa552204be28639703412945c4d874a3e
Murillo Santana <mvbentosantana@gmail.com>
2026-02-21 08:45:40 +0100

    almost nothing

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,17 +1,14 @@
 //std
-#include <cmath>
 #include <cstdlib>
 #include <stdexcept>
 
 //test
-#include "Canvas/Test/inc/Engine.hpp"
 #include "Canvas/Test/inc/examples.hpp"
 
 int32_t main(void)
 {
 	try
 	{
-		//data
 		examples::scenes::examples();
 	}
 	catch(const std::exception& e)
